3aa3592a0338ef8ac36c611b8ea7b2ad779b97ab
Ajaysai1802 <153341232+Ajaysai1802@users.noreply.github.com>
2024-07-09 15:59:23 +0530

    commit

---
+++ corejava/src/com/tnsif/interfaceprogram/Testinterface.java
@@ -0,0 +1,12 @@
+package com.tnsif.interfaceprogram;
+
+public class Testinterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank b=new SBI();
+		System.out.println("ROI="+b.);
+
+	}
+
+}
